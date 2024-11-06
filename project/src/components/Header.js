192f46531d151d40363cdfb02beeeed768361c4b
Vigneswaran Chinnasami <143638293+Vigneshxz@users.noreply.github.com>
2024-11-05 23:23:49 -0500

    changes

--- project/src/components/Header.js
+++ project/src/components/Header.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => (
   <header>
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About Us</Link></li>
-        <li><Link to="/gallery">Gallery</Link></li>
-        <li><Link to="/profile">Player Profile</Link></li>
-        <li><Link to="/schedule">Schedule</Link></li>
-        <li><Link to="/comments">Comments</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to={`${process.env.PUBLIC_URL}/`}>Home</Link></li>
+        <li><Link to={`${process.env.PUBLIC_URL}/about`}>About Us</Link></li>
+        <li><Link to={`${process.env.PUBLIC_URL}/gallery`}>Gallery</Link></li>
+        <li><Link to={`${process.env.PUBLIC_URL}/profile`}>Player Profile</Link></li>
+        <li><Link to={`${process.env.PUBLIC_URL}/schedule`}>Schedule</Link></li>
+        <li><Link to={`${process.env.PUBLIC_URL}/comments`}>Comments</Link></li>
+        <li><Link to={`${process.env.PUBLIC_URL}/contact`}>Contact</Link></li>
       </ul>
     </nav>
   </header>
 );
 
 export default Header;
-
